604e899a92c3799211a2e5345d387912dd3825ed
Jean Benkendorf <jean.benkendorf@outlook.com>
2018-10-16 20:53:34 -0300

    Campos Obrigatorios

--- WebApplication4/Data/Usuario.cs
+++ WebApplication4/Data/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,18 @@ namespace WebApplication4.Data
     public class Usuario
     {
         public int UsuarioId { get; set; }
+        [Required(ErrorMessage= "Nome é Obrigatorio")]
+        [MaxLength(30, ErrorMessage = "Nome pode conter no maximo 30 caracteres")]
         public string Nome { get; set; }
         public string Senha { get; set; }
+        [Required(ErrorMessage = "Email é Obrigatorio")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email deve estar em formato Regular")]
         public string Email { get; set; }
+        [DataType(DataType.Date)]
         public string DataNascimento { get; set; }
         public string Cpf { get; set; }
+        [Required(ErrorMessage = "CEP é Obrigatorio")]
+        [RegularExpression(@"\d{5}-d{3}", ErrorMessage = "Cep deve estar no formato 0000-000")]
         public string Cep { get; set; }
         public string Logradouro { get; set; }
         public string Numero { get; set; }
